2f64bde4c09cba096d697a04893fab7c00bc9e1c
Atohallan <wu.jungmin0929@gmail.com>
2023-03-30 15:58:23 -0700

    Bug fixes with prompt

--- src/components/pages/HomePage.vue
+++ src/components/pages/HomePage.vue
@@ -23,18 +23,20 @@ enum InputOrigin {
 }
 
 function addToChatHistory(origin: InputOrigin, input: string, responseCount: number) {
-  if (InputOrigin.user) {
-    chatHistory += `User response #${responseCount}.User input: ${input}.`
+  if (origin === InputOrigin.user) {
+    chatHistory += `User input (User response #${responseCount}): ${input}\nEnd of User response #${responseCount}.\n`
+    return
   }
 
-  if (InputOrigin.bot) {
-    chatHistory += `Bot response #${responseCount}. Input from a bot: ${input}.`
+  if (origin === InputOrigin.bot) {
+    chatHistory += `Input from a bot (Bot response #${responseCount}): ${input}\nEnd of Bot response #${responseCount}.\n`
   }
 }
 
 function clearChatHistory() {
   chatHistory = ""
   botResponseCount = 0
+  userResponseCount = 0
   chatHistoryMessageIsVisible.value = true
   previousUserInput.value = ""
   botResponse.value = ""
