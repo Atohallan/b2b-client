e6ed01ab6a1047097d42184ddc5c3b9d29238b99
Atohallan <wu.jungmin0929@gmail.com>
2023-04-01 11:20:05 -0700

    Added a footer. Moved GH btn to footer.

---
+++ src/components/layout/TheFooter.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import GithubButton from 'vue-github-button'
+</script>
+
+<template>
+  <footer>
+    <github-button href="https://github.com/bubbzDotDev/b2b-client">Code</github-button>
+  </footer>
+</template>
+
+<style scoped lang="scss">
+footer {
+  display: flex;
+  justify-content: flex-start;
+  padding: 1rem;
+}
+</style>
