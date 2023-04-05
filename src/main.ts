efdc56434de8e2bf8e3495b2c407819b40397f72
Atohallan <wu.jungmin0929@gmail.com>
2023-04-05 02:30:07 -0500

    reorganized main

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,18 @@
 import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
 import { createPinia } from 'pinia'
-
 import { plugin, defaultConfig } from '@formkit/vue'
 import '@formkit/themes/genesis'
 import { autoAnimatePlugin } from '@formkit/auto-animate/vue'
 import { createProPlugin, inputs } from '@formkit/pro'
 import '@formkit/pro/genesis'
-
-const pro = createProPlugin('fk-593a60d8ca', inputs)
-
-import App from './App.vue'
-import router from './router'
-
 import './assets/scss/index.scss'
-
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons'
 
+const pro = createProPlugin('fk-593a60d8ca', inputs)
 library.add(faMoon, faSun)
 
 const app = createApp(App)
